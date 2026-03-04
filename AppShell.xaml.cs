2433adda75c49d16ea00d7e543fc51f1380d656d
RoboticsRamon <49375983+RoboticsRamon@users.noreply.github.com>
2026-03-03 21:44:10 -0800

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BudgetCalendarApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CategoryBudgetSummary.cs
@@ -0,0 +1,20 @@
+﻿namespace BudgetCalendarApp.Models;
+
+public class CategoryBudgetSummary
+{
+    public string Name { get; set; }
+    public decimal MonthlyLimit { get; set; }
+    public decimal MonthlySpent { get; set; }
+
+    // Used by ProgressBar (0.0 – 1.0)
+    public double Progress =>
+        MonthlyLimit == 0
+            ? 0
+            : (double)(MonthlySpent / MonthlyLimit);
+
+    public string DisplayText =>
+        $"{MonthlySpent:C} / {MonthlyLimit:C}";
+
+    public bool IsOverBudget =>
+        MonthlySpent > MonthlyLimit;
+}
