b83f4af9bad6d3009140b76884c295b4660751d2
RoboticsRamon <49375983+RoboticsRamon@users.noreply.github.com>
2026-03-05 13:26:53 -0800

    Delete DailyBudgetEntry.cs

--- DailyBudgetEntry.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BudgetCalendarApp.Models;
-
-public class DailyBudgetEntry
-{
-    public DateTime Date { get; set; }
-    public string Category { get; set; }
-    public decimal Amount { get; set; }
-}
